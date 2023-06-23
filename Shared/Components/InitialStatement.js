dffd218b0c73f1f7d298179ef91a9de5fec1833c
RSBalchII <rbalchii@cnm.edu>
2023-06-22 20:55:26 -0600

    updated added 2 sections to component files

---
+++ Shared/Components/InitialStatement.js
@@ -0,0 +1,27 @@
+
+
+export function InitialStatement() {
+    return (
+        <!--section-Initial-Statement-->
+        <section class="container-lg d-flex border  mx-auto my-4">
+            <div>
+                <div class="mx-auto p-3">
+                    <ul>
+                    <strong class="white">
+                        Could growing a cannabis garden be for you?
+                    </strong>
+                        If you are already asking yourself this question then read along about the basics of what you need to get started!
+                     <strong class="white">
+                         When
+                     </strong>
+                        you decide to start your own cultivation, you will be spending less on cannabis in the long run.
+                     <strong class="white">
+                         Plus
+                     </strong>
+                        learning the basic concepts of the plant and what you need to get growing is a practical way to introduce yourself to the concept of plant phenotypes.
+                    </ul>
+                </div>
+            </div>
+        </section>
+    )
+}
