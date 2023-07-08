f20e9272c844c63335726bac499578c4946e336c
RSBalchII <rbalchii@cnm.edu>
2023-07-07 19:44:52 -0600

    updated section names

---
+++ Shared/Components/Hygiene.js
@@ -0,0 +1,44 @@
+
+
+export function Hygiene() {
+   return (
+    //section-Hygiene
+    <section class="container-lg d-flex border rounded mx-auto my-4">
+        <div class="row mx-auto">
+            <div class="col-lg-4 p-3">
+                <img class=" img-fluid rounded" alt="placeholder" src="img/HolyGrail.webp"/>
+            </div>
+            <div class="col-lg-8 mx-auto p-3">
+                <h4 id="Hygiene" class="orange">Hygiene</h4>
+                    <ul>    
+                    <strong class="orange">Health benefits of controlling your own garden.</strong> 
+                    When you grow your own garden you are in complete control of your fertilizer and pesticide usage.
+
+                    <strong class="orange">Pesticides</strong> 
+                    they come in natural and synthetic versions. 
+                    The biggest difference between the two is the method of action. Many organic methods will capture pests to incapacitate and hopefully suffocate them.
+
+                    <strong class="orange">Synthetic pesticides</strong>
+                    tend to be concentrated insect harming poisons. While there are organic poisons available as well its best to remember that poisons should  always be used as a last resort to save a harvest as cleaning a plant off of those sprayed on poisons by washing the plant down with water can cultivate mold.
+
+                    <strong class="orange">Mold/Mildew</strong>
+                    this is one of the hardest hurdles growers come across. The fungus most commonly afflicting cannabis are 
+                    
+                    <strong class="orange"> Powdery Mildew</strong> 
+                    in the 3rd stage of growth which is characterized by white furry layers first showing up underneath cannabis fan leaves and later covering the majority of fan leaves if left untended.
+
+                    Another common fungus <strong class="orange">Botrytis</strong> 
+                    commonly known as "Bud Rot" in the 4th stage of growth which is characterized by black mold forming within the flower
+                    
+                    <strong class="orange">If we look closer</strong> 
+                    there are hundreds of types of mold and mildew that love cannabis plants but all of them will show similar characteristics on the plants should you come across them.
+                    
+                    <strong class="orange">Timely watering/fertilization</strong> 
+                    dry plants can develop diseases and mutations in order to adapt to a dry environment. This can result in smaller plants, smaller flowers, and weaker trichrome production.
+                    </ul>
+            </div>
+        </div>
+    
+    </section>
+   ) 
+}
